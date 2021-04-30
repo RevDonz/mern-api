576073ac21f8700f39b3dc9f29587ff60dfe2018
RevDonz <aprilio842@gmail.com>
2021-04-30 22:59:14 +0700

    Basic Router

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('Tes');
-    console.log('yahaha');
+router.use('/product', (req, res, next) => {
+    res.json({nama: "Reva Doni Aprilio"})
 })
+
+app.use('/', router)
+
+// GET '/users' => [[nama: "Doni"]]
 
 app.listen(4000);
