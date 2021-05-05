f42188840dec03164f9e042e4f69b1801d60151d
RevDonz <aprilio842@gmail.com>
2021-05-05 14:28:58 +0700

    Install mongoose

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,15 +1,27 @@
+const {validationResult} = require('express-validator');
+
+
 exports.createBlogPost = (req, res, next) => {
     const title = req.body.title;
     // const image = req.body.image;
     const body = req.body.body;
 
+    const error = validationResult(req)
+
+    if (!error.isEmpty()) {
+        const err = new Error('Input value tidak sesuai');
+        err.errorStatus = 400;
+        err.data = error.array();
+        throw err;
+    }
+
     const result = {
         message: "Create Blog Post Success",
         data: {
             post_id: 1,
-            title: "Title Blog",
-            // image: "image.png",
-            body: "Lorem Ipsum Dolor Sit Amet",
+            title: title,
+            image: "image.png", 
+            body: body,
             created_at: "03/05/2021",
             author: {
                 uid: 1,
